8ead9982bad7488f38015b356e730149c915b6c9
VineBast <vine.bast@gmail.com>
2022-06-15 13:24:10 +0200

    add google api

--- src/screens/ItineraryScreen.js
+++ src/screens/ItineraryScreen.js
@@ -3,6 +3,7 @@ import { StyleSheet, View } from 'react-native';
 import { BackButton, Background, OptionButton, Paragraph } from '../components';
 import { colors, general } from '../core/theme';
 import { getUser } from '../services/firebase';
+import MapView from 'react-native-maps';
 
 export function ItineraryScreen({ navigation }) {
   const [user, setUser] = useState(undefined);
@@ -15,6 +16,14 @@ export function ItineraryScreen({ navigation }) {
     <Background>
       <BackButton goBack={navigation.goBack} />
       <OptionButton navigation={navigation} />
+      <MapView
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
       <Paragraph>Voici l'itineraire</Paragraph>
       <View style={style.modalClose}>
         <Paragraph>Coucou</Paragraph>
